576591459614b8fe11aa8d054581a315f6e476fb
Tinnnapon C <tinnnapon.chotepachroj@gmail.com>
2023-07-12 00:33:40 +0700

    add order+compo

--- src/api/order/controllers/order.js
+++ src/api/order/controllers/order.js
@@ -1,3 +1,4 @@
+// @ts-nocheck
 "use strict";
 
 /**
@@ -10,15 +11,22 @@ module.exports = createCoreController("api::order.order", ({ strapi }) => ({
   async addOrder(ctx) {
     let param = ctx.request.body;
     // console.log(param);
-    let data = await strapi.db.query("api::order.order").create({
+    let data = await strapi.entityService.create("api::order.order", {
       data: {
-        // publishedAt: { $notNull: true },
-        //   type: param.type,
         status: "open",
         user: param.user,
         branch: { id: param.branch },
-        foodOrder: param.foodOrder,
-        // $or: [{ inactive_at: null }, { inactive_at: { $gt: _dateNow } }],
+        listOrder: param.listOrder,
+      },
+    });
+    return data;
+  },
+  async findOrder(ctx) {
+    let param = ctx.request.body;
+    // console.log(param);
+    let data = await strapi.db.query("api::order.order").findMany({
+      populate: {
+        listOrder: { populate: true },
       },
     });
     return data;
--- src/api/order/routes/order.js
+++ src/api/order/routes/order.js
@@ -9,5 +9,6 @@ const { createCoreRouter } = require("@strapi/strapi").factories;
 module.exports = {
   routes: [
     { method: "POST", path: "/order/addOrder", handler: "order.addOrder" },
+    { method: "GET", path: "/order/getOrder", handler: "order.findOrder" },
   ],
 };
